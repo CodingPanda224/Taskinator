4760e589955db67f7ebb527f73e0b66cf66487ef
Panda <juanck224@gmail.com>
2020-09-17 16:39:12 -0400

    add task branch

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// var buttonEl = document.querySelector("#save-task");
+// console.log(buttonEl);
+
+var taskItemEl = document.createElement("li");
+taskItemEl.textContent = "hello";
+taskItemEl;
+
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+tasksToDoEl;
+var taskItemEl = document.createElement("li");
+taskItemEl.textContent = "hello";
+tasksToDoEl.appendChild(taskItemEl);
+taskItemEl.className = "task-item";
